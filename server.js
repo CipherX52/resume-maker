76a0ee3428d7880ae71bb6a7f9042f4478b04d4f
CipherX52 <32170371+CipherX52@users.noreply.github.com>
2022-03-03 21:52:59 +0530

    db connect

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/auth", require("./routes/auth"));
 
+const mongoose = require('mongoose');
+
+const connectDB = async() => {
+    await mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,
+        useUnifiedTopology:true
+    });
+
+    console.log("MongoDB connected");
+}
+
+connectDB();
 
 //after frontend production build is built, uncomment the 4 lines below
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
