0583b2298107e38557b3557019a4709c42c75972
ashin444 <93370941+ashin444@users.noreply.github.com>
2022-03-09 11:03:24 +0530

    frontend
    frontend codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import SignUp from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import About from "./components/about/About";
+import FormPage from "./components/form/FormPage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </>
+    </Router >
   );
 }
+
 
 export default App;
