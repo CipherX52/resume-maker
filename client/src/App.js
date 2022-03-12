413e470a313b11ddd339da6c103b0d71721e9523
ashin444 <93370941+ashin444@users.noreply.github.com>
2022-03-12 14:33:16 +0530

    page not found page
    added as error

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import SignUp from "./components/signup/Signup";
 import Login from "./components/login/Login";
 import About from "./components/about/About";
 import FormPage from "./components/form/FormPage";
-
+import Error from "./components/error/Error";
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/form" element={<FormPage />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </>
     </Router >
