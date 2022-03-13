1ef9233ab4306d18ced1738d96b11800628004c8
ashin444 <93370941+ashin444@users.noreply.github.com>
2022-03-13 10:50:38 +0530

    admin
    admin dashboard

---
+++ client/src/components/admin/Admin.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+import Footer from "./AdminComponents/Footer";
+import HearderComponent from "./AdminComponents/Header/HearderComponent";
+
+function App() {
+  return (
+    <>
+      <HearderComponent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
