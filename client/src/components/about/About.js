3b05057bc5e9787102eb5703866cd84fbdc96793
shamily1994 <83786125+shamily1994@users.noreply.github.com>
2022-03-13 16:10:57 +0530

    Update About.js

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -13,12 +13,12 @@ export default function About() {
           Digital Resume builders
         </h4>
        
-        <p> <span>We are </span>the digital resume building  platform which features the design to help you win your dream job.</p>
-         <p> <span>Easy online resume builder, </span>Create an awesome resume, online profile without leaving your web browser. </p> 
+        <p> <span>We are </span>the digital resume building  platform.</p>
+         <p> <span>Easy online resume builder, </span>Create an awesome resume.</p> 
 
         <p><span>Your data is safe, </span> The data is kept private and protected.</p>
                 
-        <p><span>Approved templates, </span>Professionally-designed resume templates. Just edit and download in 5 minutes </p>
+        <p><span>Approved templates, </span>Professionally-designed resume. </p>
         <h2>User-friendly. Professional. Effective. Try our resume builder today!!!</h2>
         {/* <Link to="/back">
           <button></button>
